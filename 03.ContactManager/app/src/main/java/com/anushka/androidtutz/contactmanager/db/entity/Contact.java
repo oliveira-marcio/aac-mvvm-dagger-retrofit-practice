dc4dfea09f6e9b21594b79c7eafdffdd81a2ef13
Márcio <emaildomso@gmail.com>
2019-02-25 08:28:39 -0300

    refactor: Add Room library and create Entity class

--- 03.ContactManager/app/src/main/java/com/anushka/androidtutz/contactmanager/db/entity/Contact.java
+++ 03.ContactManager/app/src/main/java/com/anushka/androidtutz/contactmanager/db/entity/Contact.java
@@ -1,18 +1,29 @@
 package com.anushka.androidtutz.contactmanager.db.entity;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "contacts")
 public class Contact {
 
-    public static final String TABLE_NAME = "contacts";
+//    public static final String TABLE_NAME = "contacts";
 
-    public static final String COLUMN_ID = "contact_id";
-    public static final String COLUMN_NAME = "contact_name";
-    public static final String COLUMN_EMAIL = "contact_email";
+//    public static final String COLUMN_ID = "contact_id";
+//    public static final String COLUMN_NAME = "contact_name";
+//    public static final String COLUMN_EMAIL = "contact_email";
 
+    @ColumnInfo(name = "contact_name")
     private String name;
+    @ColumnInfo(name = "contact_email")
     private String email;
+    @ColumnInfo(name = "contact_id")
+    @PrimaryKey(autoGenerate = true)
     private long id;
 
 
+    @Ignore
     public Contact() {
     }
 
@@ -50,10 +61,10 @@ public class Contact {
 
 
     // Create table SQL query
-    public static final String CREATE_TABLE =
-            "CREATE TABLE " + TABLE_NAME + "("
-                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                    + COLUMN_NAME + " TEXT,"
-                    + COLUMN_EMAIL + " DATETIME DEFAULT CURRENT_TIMESTAMP"
-                    + ")";
+//    public static final String CREATE_TABLE =
+//            "CREATE TABLE " + TABLE_NAME + "("
+//                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+//                    + COLUMN_NAME + " TEXT,"
+//                    + COLUMN_EMAIL + " DATETIME DEFAULT CURRENT_TIMESTAMP"
+//                    + ")";
 }
