fc2181b210b79b9331d8c688e6bfc6c8b315baa2
Márcio <emaildomso@gmail.com>
2019-03-02 08:32:41 -0300

    refactor: Add DataBing to main activity

--- 04.BindingDemo1/app/src/main/java/com/anushka/androidtutz/bindingdemo1/MainActivity.java
+++ 04.BindingDemo1/app/src/main/java/com/anushka/androidtutz/bindingdemo1/MainActivity.java
@@ -1,27 +1,26 @@
 package com.anushka.androidtutz.bindingdemo1;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.anushka.androidtutz.bindingdemo1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView nameText;
     private TextView emailText;
 
-
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nameText=findViewById(R.id.tvStudentName);
-        emailText=findViewById(R.id.tvStudentEmail);
-
-        nameText.setText(getCurrentStudent().getStudentName());
-        emailText.setText(getCurrentStudent().getStudentEmail());
-
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setStudent(getCurrentStudent());
 
     }
 
