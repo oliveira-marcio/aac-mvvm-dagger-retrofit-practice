fdd1e2a043ef2a45260f7906b77b9264494da670
Márcio <emaildomso@gmail.com>
2019-03-02 12:30:41 -0300

    feat: Add Two-Way Data Binding

--- 06.TwoWayDemo/app/src/main/java/com/anushka/androidtutz/twowaydemo/Contact.java
+++ 06.TwoWayDemo/app/src/main/java/com/anushka/androidtutz/twowaydemo/Contact.java
@@ -1,6 +1,9 @@
 package com.anushka.androidtutz.twowaydemo;
 
-public class Contact {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class Contact extends BaseObservable {
 
     private int id;
     private String name;
@@ -11,27 +14,33 @@ public class Contact {
         this.email = email;
     }
 
+    @Bindable
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
+        notifyPropertyChanged(BR.id);
     }
 
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
+    @Bindable
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+        notifyPropertyChanged(BR.email);
     }
 }
