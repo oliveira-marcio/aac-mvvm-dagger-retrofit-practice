a43a760c6e7ca8191ea64d0be57cd5e965d24d33
Márcio <emaildomso@gmail.com>
2019-03-06 09:45:36 -0300

    feat: Add new functional activity to add books

--- 08.EBookShop/app/src/main/java/com/marcio/ebookshop/MainActivity.java
+++ 08.EBookShop/app/src/main/java/com/marcio/ebookshop/MainActivity.java
@@ -2,16 +2,18 @@ package com.marcio.ebookshop;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 import com.marcio.ebookshop.databinding.ActivityMainBinding;
 import com.marcio.ebookshop.model.Book;
@@ -30,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     private Category selectedCategory;
     private RecyclerView booksRecyclerView;
     private BooksAdapter booksAdapter;
+    private int selectedBookId;
+
+    public static final int ADD_BOOK_REQUEST_CODE = 1;
+    public static final int EDIT_BOOK_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,17 +82,69 @@ public class MainActivity extends AppCompatActivity {
         booksRecyclerView.setAdapter(booksAdapter);
 
         booksAdapter.setBooks(booksList);
+        booksAdapter.setListener(new BooksAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Book book) {
+                selectedBookId = book.getBookId();
+                Intent intent = new Intent(MainActivity.this, AddAndEditActivity.class);
+                intent.putExtra(AddAndEditActivity.BOOK_ID, selectedBookId);
+                intent.putExtra(AddAndEditActivity.BOOK_NAME, book.getBookName());
+                intent.putExtra(AddAndEditActivity.BOOK_PRICE, book.getUnitPrice());
+                startActivityForResult(intent, EDIT_BOOK_REQUEST_CODE);
+            }
+        });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                Book bookToDelete = booksList.get(viewHolder.getAdapterPosition());
+                mainActivityViewModel.deleteNewBook(bookToDelete);
+
+            }
+        }).attachToRecyclerView(booksRecyclerView);
     }
 
     public class MainActivityClickHandlers {
 
         public void onFABClicked(View view) {
-            Toast.makeText(MainActivity.this, "FAB Clicked", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, AddAndEditActivity.class);
+            startActivityForResult(intent, ADD_BOOK_REQUEST_CODE);
         }
 
         public void onSelectItem(AdapterView<?> parent, View view, int pos, long id) {
             selectedCategory = (Category) parent.getItemAtPosition(pos);
             loadBooksList(selectedCategory.getId());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        int selectedCategoryId = selectedCategory.getId();
+
+        Book book = new Book();
+        book.setCategoryId(selectedCategoryId);
+        book.setBookName(data.getStringExtra(AddAndEditActivity.BOOK_NAME));
+        book.setUnitPrice(data.getStringExtra(AddAndEditActivity.BOOK_PRICE));
+
+        switch (requestCode) {
+            case ADD_BOOK_REQUEST_CODE:
+                mainActivityViewModel.addNewBook(book);
+                break;
+            case EDIT_BOOK_REQUEST_CODE:
+                book.setBookId(selectedBookId);
+                mainActivityViewModel.updateNewBook(book);
+                break;
         }
     }
 }
