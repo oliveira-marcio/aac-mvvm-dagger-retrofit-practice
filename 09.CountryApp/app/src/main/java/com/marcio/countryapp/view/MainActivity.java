9105490b3b2bce2926e9618dc3ab0035ec8d764d
Márcio <emaildomso@gmail.com>
2019-03-09 07:28:50 -0300

    feat: Create model classes for API

--- 09.CountryApp/app/src/main/java/com/marcio/countryapp/view/MainActivity.java
+++ 09.CountryApp/app/src/main/java/com/marcio/countryapp/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.marcio.countryapp;
+package com.marcio.countryapp.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.marcio.countryapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
