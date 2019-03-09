de532ddb096f3a847fedfb35682b817374cb806a
Márcio <emaildomso@gmail.com>
2019-03-09 07:43:43 -0300

    feat: Create Retrofit instance

---
+++ 09.CountryApp/app/src/main/java/com/marcio/countryapp/service/RetrofitInstance.java
@@ -0,0 +1,21 @@
+package com.marcio.countryapp.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+    private static Retrofit retrofit = null;
+    public static String BASE_URL = "http://services.groupkt.com/";
+
+    public static GetCountryDataService getService() {
+        if (retrofit == null) {
+            retrofit = new Retrofit
+                    .Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(GetCountryDataService.class);
+    }
+}
