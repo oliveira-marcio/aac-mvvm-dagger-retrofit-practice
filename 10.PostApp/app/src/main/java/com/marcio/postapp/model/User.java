c47670a965632dd8909b85aae166ae68353df355
Márcio <emaildomso@gmail.com>
2019-03-12 07:01:40 -0300

    feat: Start PostApp exercise with model

---
+++ 10.PostApp/app/src/main/java/com/marcio/postapp/model/User.java
@@ -0,0 +1,31 @@
+package com.marcio.postapp.model;
+
+public class User {
+    private String userEmail;
+    private String password;
+    private int id;
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
