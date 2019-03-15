8dcc09a6fb32e97dc56853b0e5cfd77dd7c746d4
Márcio Oliveira <emaildomso@gmail.com>
2019-03-15 15:40:02 -0300

    refactor: Modify MainActivity for Paging

--- 11.TMDBClient/app/src/main/java/com/androidtutz/anushka/tmdbclient/service/MovieDataService.java
+++ 11.TMDBClient/app/src/main/java/com/androidtutz/anushka/tmdbclient/service/MovieDataService.java
@@ -11,12 +11,7 @@ import retrofit2.http.Query;
  */
 public interface MovieDataService {
 
-
     @GET("movie/popular")
-    Call<MovieDBResponse> getPopularMovies(@Query("api_key") String apiKey);
-
-    @GET("movie/popular")
-    Call<MovieDBResponse> getPopularMoviesWithPaging(@Query("api_key") String apiKey,@Query("page") long page);
-
+    Call<MovieDBResponse> getPopularMoviesWithPaging(@Query("api_key") String apiKey, @Query("page") long page);
 
 }
--- 11.TMDBClient/app/src/main/java/com/androidtutz/anushka/tmdbclient/viewmodel/MainActivityViewModel.java
+++ 11.TMDBClient/app/src/main/java/com/androidtutz/anushka/tmdbclient/viewmodel/MainActivityViewModel.java
@@ -10,48 +10,38 @@ import android.support.annotation.NonNull;
 import com.androidtutz.anushka.tmdbclient.model.Movie;
 import com.androidtutz.anushka.tmdbclient.model.MovieDataSource;
 import com.androidtutz.anushka.tmdbclient.model.MovieDataSourceFactory;
-import com.androidtutz.anushka.tmdbclient.model.MovieRepository;
 import com.androidtutz.anushka.tmdbclient.service.MovieDataService;
 import com.androidtutz.anushka.tmdbclient.service.RetrofitInstance;
 
-import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class MainActivityViewModel extends AndroidViewModel {
-    private MovieRepository movieRepository;
-
     LiveData<MovieDataSource> movieDataSourceLiveData;
     private Executor executor;
     private LiveData<PagedList<Movie>> moviesPagedList;
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
-        movieRepository=new MovieRepository(application);
 
-        MovieDataService movieDataService= RetrofitInstance.getService();
-        MovieDataSourceFactory factory=new MovieDataSourceFactory(movieDataService,application);
-        movieDataSourceLiveData=factory.getMutableLiveData();
+        MovieDataService movieDataService = RetrofitInstance.getService();
+        MovieDataSourceFactory factory = new MovieDataSourceFactory(movieDataService, application);
+        movieDataSourceLiveData = factory.getMutableLiveData();
 
-         PagedList.Config config=(new PagedList.Config.Builder())
-                                   .setEnablePlaceholders(true)
-                                   .setInitialLoadSizeHint(10)
-                                   .setPageSize(20)
-                                   .setPrefetchDistance(4)
-                                    .build();
+        PagedList.Config config = (new PagedList.Config.Builder())
+                .setEnablePlaceholders(true)
+                .setInitialLoadSizeHint(10)
+                .setPageSize(20)
+                .setPrefetchDistance(4)
+                .build();
 
-         executor= Executors.newFixedThreadPool(5);
+        executor = Executors.newFixedThreadPool(5);
 
-         moviesPagedList = (new LivePagedListBuilder<Long,Movie>(factory,config))
-                 .setFetchExecutor(executor)
-                 .build();
+        moviesPagedList = (new LivePagedListBuilder<Long, Movie>(factory, config))
+                .setFetchExecutor(executor)
+                .build();
 
 
-    }
-
-    public LiveData<List<Movie>> getAllMovies(){
-
-        return movieRepository.getMutableLiveData();
     }
 
     public LiveData<PagedList<Movie>> getMoviesPagedList() {
