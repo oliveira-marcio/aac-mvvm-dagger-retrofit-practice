024b82eb8ddf76c6664f6e009635796a471111d6
Márcio Oliveira <emaildomso@gmail.com>
2019-03-21 18:28:41 -0300

    refactor: Working with modules

--- 12.DIDemo1/app/src/main/java/com/androidtutz/anushka/didemo/MemoryCard.java
+++ 12.DIDemo1/app/src/main/java/com/androidtutz/anushka/didemo/MemoryCard.java
@@ -2,9 +2,9 @@ package com.androidtutz.anushka.didemo;
 
 import javax.inject.Inject;
 
+// Simulating the scenario where we don't own MemoryCard class
 public class MemoryCard {
 
-    @Inject
     public MemoryCard() {
     }
 }
